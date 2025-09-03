4dd53205c0c4911eb068ca2e8d4ba68c1d18038f
yominax <sidimoh.mouta@gmail.com>
2025-09-03 19:25:09 +0200

    Changement de region

--- include/terraform/main.tf
+++ include/terraform/main.tf
@@ -6,7 +6,7 @@ resource "random_pet" "prefix" {
 # Azure Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "${random_pet.prefix.id}-rg-data-pipeline"
-  location = "westeurope"
+  location = "centralindia"
 }
 
 data "azurerm_client_config" "current" {}
